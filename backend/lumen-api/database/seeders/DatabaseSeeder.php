5ead4158ce8b107f43512dc9a051fbd0f0332f1a
Hugo Piat <h.piat@outlook.fr>
2025-02-09 01:11:33 +0100

    debug

--- backend/lumen-api/database/seeders/DatabaseSeeder.php
+++ backend/lumen-api/database/seeders/DatabaseSeeder.php
@@ -1,19 +1,17 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('Password123')
+        ]);
     }
 }
